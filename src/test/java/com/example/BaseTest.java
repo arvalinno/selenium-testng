2b16b3275e44cf664c5038b8467a20b80e81d4c1
arvalinno <arvalinno@gmail.com>
2025-07-14 18:20:04 +0700

    add config use selenium-standalone container

--- src/test/java/com/example/BaseTest.java
+++ src/test/java/com/example/BaseTest.java
@@ -1,19 +1,21 @@
 package com.example;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.TestNG;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 
 public abstract class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setUp() {
+    public void setUp() throws Exception {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
@@ -21,7 +23,15 @@ public abstract class BaseTest {
         options.addArguments("--disable-gpu");
 
         // driver = new ChromeDriver(options);
-        driver = new ChromeDriver();
+        try {
+            options = new ChromeOptions();
+            driver = new RemoteWebDriver(new URL("http://selenium:4444/wd/hub"), options);
+            System.out.println("herer loll");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Invalid Selenium Grid URL");
+        }
+        // driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
     }
 
